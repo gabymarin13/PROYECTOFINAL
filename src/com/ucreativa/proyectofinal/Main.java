f9686d62f3e0becc43416767c3e683f579fb9f41
gabymarin13 <ana.marin.chaves@ucreativa.com>
2021-06-21 00:01:18 -0600

    Commit completo proyecto final - Gabriela Marin

--- src/com/ucreativa/proyectofinal/Main.java
+++ src/com/ucreativa/proyectofinal/Main.java
@@ -5,7 +5,6 @@ import com.ucreativa.proyectofinal.ui.FrontEnd;
 public class Main {
 
     public static void main(String[] args) {
-        int x = 0;
         FrontEnd pantalla = new FrontEnd("Registro de ingresos diarios al salón");
         pantalla.build();
     }
